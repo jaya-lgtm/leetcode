a34926be1dbdff770ad15e1f757ea69ff759b9bc
jaya-lgtm <gudesrekrishna14@gmail.com>
2026-08-07 20:44:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (20.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -5,21 +5,24 @@ public:
         long long res=0;
         int i=0;
         int sign=1;
-        
         while(i<n && s[i]==' ') i++;
-        if( i<n && (s[i]=='-' || s[i]=='+')){
-            if(s[i]=='-') sign=-1;
-            i++;
+        if((i<n && s[i]=='-')||s[i]=='+'){
+            if(s[i]=='-') {
+                sign=-1;
+                }
+                    i++;
         }
-        while(i<n && (s[i]>='0' && s[i]<='9')){
-            int digit=s[i]-'0';
-            if(res>INT_MAX/10LL || (res==INT_MAX/10LL && digit>(sign==1?7:8))){
-               return sign==1?INT_MAX:INT_MIN;
-            }
-            res=res*10+digit;
-            i++;
-        }
-        res= res*sign;
-        return (int)res;
+       
+    
+    while(i<n && s[i]>='0' && s[i]<='9'){
+        int digit=s[i]-'0';
+        if(res>INT_MAX/10LL||(res==INT_MAX/10LL && digit>(sign==1?7:8)))
+        return sign==1?INT_MAX:INT_MIN;
+        res=res*10+digit;
+        i++;
     }
+    res*=sign;
+    return (int) res;
+    }
+
 };
